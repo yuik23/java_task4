ae07ac94315e08641eec1d65b59ed43a2a3f3937
yuik23 <yui_karamatsu@yahoo.co.jp>
2023-02-10 07:09:33 +0900

    initial commit

---
+++ src/StreamSample.java
@@ -0,0 +1,15 @@
+import java.util.Comparator;
+import java.util.List;
+
+public class StreamSample {
+
+  public static void main(String[] args) {
+    List<String> names = List.of("yamada", "tanaka", "yasuda", "suzuki", "iida");
+    System.out.println("1.降順に並び替え");
+    names.stream().sorted(Comparator.reverseOrder()).forEach(System.out::println);
+    System.out.println("2.iで始まる人数");
+    System.out.println(names.stream().filter(name -> name.startsWith("i")).count());
+    System.out.println("3.suzukiさんはいるか");
+    System.out.println(names.stream().anyMatch(name -> name.equals("suzuki")));
+  }
+}
